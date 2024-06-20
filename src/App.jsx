2442efaca91f502bfcd686b722e76480ac7b87e5
Lourdes <lievalenzuela21@gmail.com>
2024-06-19 23:51:56 -0400

    Usando JSX

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ListaTareas from './componentes/ListaTareas';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ListaTareas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/ListaTareas.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ListaTareas = () => {
+  return (
+    <>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+};
+
+export default ListaTareas;
